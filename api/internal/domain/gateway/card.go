e01c3574dd0bfd0415cdff2470f9416ba8ab75f1
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2023-10-22 14:59:51 -0300

    Feat: Created card gateway

---
+++ api/internal/domain/gateway/card.go
@@ -0,0 +1,11 @@
+package gateway
+
+import "github.com/marcelocampanelli/trello-clone/internal/domain/entity"
+
+type CardGateway interface {
+	Create(card *entity.Card) (*entity.Card, error)
+	Update(id string, card *entity.Card) (*entity.Card, error)
+	Delete(id string) error
+	FindByID(id string) (*entity.Card, error)
+	FindAll(listID string) ([]*entity.Card, error)
+}
