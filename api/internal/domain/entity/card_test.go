6403b2abc859f2ba19befd9563b5c41e164915ac
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2023-10-22 14:54:24 -0300

    Feat: Improvments in card tests to entity

--- api/internal/domain/entity/card_test.go
+++ api/internal/domain/entity/card_test.go
@@ -1,23 +1,33 @@
 package entity
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestCard_NewCard(t *testing.T) {
-	card, err := NewCard("test")
+	card, err := NewCard("test", "01")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, card)
+}
+
+func TestCard_NewCard_Invalid(t *testing.T) {
+	card, err := NewCard("", "")
+
+	assert.NotNil(t, err)
+	assert.Nil(t, card)
+}
+
+func TestCard_Modify(t *testing.T) {
+	card, err := NewCard("test", "01")
 
 	assert.Nil(t, err)
 	assert.NotNil(t, card)
 
-	fmt.Println(card)
-}
+	err = card.Modify("test2", "02")
 
-func TestCard_NewCard_Invalid(t *testing.T) {
-	card, err := NewCard("")
-
-	assert.NotNil(t, err)
-	assert.Nil(t, card)
+	assert.Nil(t, err)
+	assert.Equal(t, "test2", card.Name)
+	assert.Equal(t, "02", card.UserAssignedID)
 }
